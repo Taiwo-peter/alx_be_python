76bf187ad728dfa2b3ed340c9896a4d1d68f3d77
Taiwo-peter <taiwofaladepeters@gmail.com>
2025-09-16 00:11:58 +0100

    basic_operations commit

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+print(addition)
+subtraction = number1 - number2
+print(subtraction)
+multiplication = number1 * number2
+print(multiplication)
