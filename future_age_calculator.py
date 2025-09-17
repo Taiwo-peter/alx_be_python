03cceeca69ae7f00ae6b28b2044016584f807564
Taiwo-peter <taiwofaladepeters@gmail.com>
2025-09-17 17:50:27 +0100

    added other phython files

---
+++ future_age_calculator.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your current age: "))
+current_year = 2023
+future_year = 2050
+future_age = age + (future_year - current_year)
+print(f"In the year {future_year}, you will be {future_age} years old.")
