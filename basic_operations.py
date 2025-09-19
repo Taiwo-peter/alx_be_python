738d13d4a5a67b394ce4ab64e3d71b738c85ad4f
Taiwo-peter <taiwofaladepeters@gmail.com>
2025-09-19 23:15:09 +0100

    all itemes pushed inside python_introduction

--- basic_operations.py
+++
@@ -1,9 +0,0 @@
-number1 = 10
-number2 = 5
-
-addition = number1 + number2
-print(addition)
-subtraction = number1 - number2
-print(subtraction)
-multiplication = number1 * number2
-print(multiplication)
